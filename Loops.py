151d1040a857005e9bbbece682b7201db7eb89af
William Aponte <apontewilliam16@gmail.com>
2020-04-22 12:34:23 -0400

    Third Commit

---
+++ Loops.py
@@ -0,0 +1,34 @@
+#Loops 
+# nums = [1,2,3,4,5]
+#for num in nums:
+    #if num == 3: 
+        #print('Found!', + num)
+        #continue 
+        #break
+    #print(num)
+
+# Nested loops 
+# nums = [1,2,3,4,5]
+#for num in nums:
+   # for letter in 'abc':
+       #print(num , letter)
+
+# Range
+#for i in range(1, 6): #
+    #print(i)
+
+
+# While loops 
+
+x = 0  #initializing the Loop
+
+while True:
+ # while  x < 10 :
+    if x == 5:
+       break
+    print(x)
+    x += 1 
+    # same as 
+    # x + 1 = x
+
+# To stop an infinite loop you have to state the break Statement to be able to stop. 
